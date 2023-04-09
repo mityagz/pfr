771bbc1ace3d5e0f9aca593a8fd1e5abc949e8a7
mityagz <mityagz@gmail.com>
2023-04-09 11:24:51 +0500

    add handler for advertise/don't

--- src/ping_readloop.cpp
+++ src/ping_readloop.cpp
@@ -8,7 +8,6 @@ extern int req_stopped;
 
 
 void * readloop(void  *ina) {
-//void readloop(void) {
 	int				size;
 	char			recvbuf[BUFSIZE];
 	char			controlbuf[BUFSIZE];
@@ -21,7 +20,6 @@ void * readloop(void  *ina) {
     struct proto    proto_v4 = { proc_v4, NULL, NULL, NULL, NULL, 0, IPPROTO_ICMP };
     pr = &proto_v4;
 
-	//int sockrd = Socket(pr->sasend->sa_family, SOCK_RAW, pr->icmpproto);
 	int sockrd = Socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 
 	setuid(getuid());		/* don't need special permissions any more */
@@ -31,7 +29,10 @@ void * readloop(void  *ina) {
 	size = 60 * 1024;		/* OK if setsockopt fails */
 	setsockopt(sockrd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
 
-	//sig_alrm(SIGALRM);		/* send first packet */
+    int flags;
+    fcntl(sockrd, F_SETFL, flags | O_NONBLOCK);
+    struct timeval stv;
+    int sres;
 
 	iov.iov_base = recvbuf;
 	iov.iov_len = sizeof(recvbuf);
@@ -40,28 +41,38 @@ void * readloop(void  *ina) {
 	msg.msg_iovlen = 1;
 	msg.msg_control = controlbuf;
 	for ( ; ; ) {
-		//msg.msg_namelen = pr->salen;
-		msg.msg_controllen = sizeof(controlbuf);
-		n = recvmsg(sockrd, &msg, 0);
-		if (n < 0) {
-			if (errno == EINTR)
-				continue;
-			else
-				err_sys("recvmsg error");
-		}
+        fd_set readset;
+        FD_ZERO(&readset);
+        FD_SET(sockrd, &readset);
+        int maxfd = sockrd;
+        stv.tv_sec = 1;
+        stv.tv_usec = 0;
+        sres = select(maxfd + 1, &readset, NULL, NULL, &stv);
 
-		Gettimeofday(&tval, NULL);
-        pthread_mutex_lock(&mtr);
-		(*pr->fproc)(recvbuf, n, &msg, &tval);
-        pthread_mutex_unlock(&mtr);
-        
+        if (sres == -1) {
+        } else if (sres) {
+		    //msg.msg_namelen = pr->salen;
+		    msg.msg_controllen = sizeof(controlbuf);
+		    if(FD_ISSET(sockrd, &readset)) {
+                n = recvmsg(sockrd, &msg, 0);
+		        if (n < 0) {
+			        if (errno == EINTR)
+				        continue;
+			        else
+				        err_sys("recvmsg error");
+		        }
+
+		        Gettimeofday(&tval, NULL);
+                pthread_mutex_lock(&mtr);
+		        (*pr->fproc)(recvbuf, n, &msg, &tval);
+                pthread_mutex_unlock(&mtr);
+            }
+        }
         pthread_mutex_lock(&mt_req_send);
         if(req_stopped == 1) {
             send_stopped = 1;
         }
         pthread_mutex_unlock(&mt_req_send);
-    /*
-      */
 	}
     return NULL;
 }
