e94746d834270df7b7b4c3f53fec587cf3c51ffa
mityagz <mityagz@gmail.com>
2025-09-17 15:19:00 +0500

    add sql queries

--- contrib/queries.sql
+++ contrib/queries.sql
@@ -1,6 +1,6 @@
 -- 1.
 -- num of uniq addresses
-select count(distinct  ipv4_dest_address)
+select count(distinct ipv4_dest_address)
 from pfr_log;
 
 -- 2.
@@ -10,3 +10,9 @@ from pfr_log
 group by ipv4_dest_address 
 order by ipv4_dest_address;
 
+-- 3.
+-- select all
+select * 
+from pfr_log l, pfr_peers p 
+where l.peer_id = p.id
+order by ipv4_dest_address;
