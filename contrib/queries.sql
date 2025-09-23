93630c5a43c454abaed4d7c4bf5dcacc1f9dc13a
mityagz <mityagz@gmail.com>
2025-09-23 12:16:33 +0500

    add sql queries

--- contrib/queries.sql
+++ contrib/queries.sql
@@ -41,3 +41,47 @@ hostname | peer_id | num_prefixes_per_peer
  IPFIX1   |      10 |                     5
 (8 rows)
 */
+
+-- 6. This query shows num prefixes per peer w/stats which pfrd send to gobgpd
+select n.hostname, l.peer_id, count(l.ipv4_dest_address) as num_prefixes_per_peer, sum(rtt_min) / count(rtt_min) as avg_rtt_min, 
+       sum(pkt_loss) / count(pkt_loss) as avg_pkt_loss
+from pfr_log l
+join pfr_peers p on l.peer_id = p.id
+join node n on p.node_id = n.id 
+where probe_id in (select max(probe_id) from pfr_log l) group by n.hostname, l.peer_id;
+
+/*
+hostname | peer_id | num_prefixes_per_peer |   avg_rtt_min    | avg_pkt_loss 
+----------+---------+-----------------------+------------------+--------------
+ IPFIX0   |       9 |                     5 | 77.6192016601562 |            1
+ PE0      |       6 |                     5 | 62.5815979003906 |            0
+ PE1      |       4 |                     1 | 2.01200008392334 |            2
+ PE0      |       2 |                     3 | 50.0956624348958 |            1
+ PE1      |       8 |                     2 | 74.6735000610352 |            1
+ PE0      |       5 |                     3 | 51.0523376464844 |            1
+ PE1      |       7 |                     4 |  72.619499206543 |            2
+ IPFIX1   |      10 |                     4 | 56.4552459716797 |            1
+(8 rows)
+*/
+
+-- 7. This query shows num prefixes per peer w/stats and round which pfrd send to gobgpd
+select n.hostname, l.peer_id, count(l.ipv4_dest_address) as num_prefixes_per_peer, round(cast(sum(rtt_min) / count(rtt_min) as numeric), 2) as avg_rtt_min,
+       sum(pkt_loss) / count(pkt_loss) as avg_pkt_loss
+from pfr_log l
+join pfr_peers p on l.peer_id = p.id
+join node n on p.node_id = n.id  
+where probe_id in (select max(probe_id) from pfr_log l) group by n.hostname, l.peer_id;
+
+/*
+hostname | peer_id | num_prefixes_per_peer | avg_rtt_min | avg_pkt_loss 
+----------+---------+-----------------------+-------------+--------------
+ IPFIX0   |       9 |                     3 |       76.05 |            1
+ PE0      |       6 |                     3 |        2.42 |            1
+ PE1      |       4 |                     5 |       76.76 |            1
+ PE0      |       2 |                     7 |       77.05 |            1
+ PE1      |       8 |                     2 |       77.65 |            1
+ PE0      |       5 |                     4 |       58.75 |            1
+ PE1      |       7 |                     1 |       72.61 |            1
+ IPFIX1   |      10 |                     2 |       39.93 |            2
+(8 rows)
+*/
