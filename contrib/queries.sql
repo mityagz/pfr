4202c05372f3ce53b1932c209067ff0e9a8aa520
mityagz <mityagz@gmail.com>
2025-09-24 19:34:45 +0500

    add sql queries

--- contrib/queries.sql
+++ contrib/queries.sql
@@ -85,3 +85,57 @@ hostname | peer_id | num_prefixes_per_peer | avg_rtt_min | avg_pkt_loss
  IPFIX1   |      10 |                     2 |       39.93 |            2
 (8 rows)
 */
+
+-- 8. This query shows num prefixes per peer w/stats/metric  and round which pfrd send to gobgpd
+select n.hostname, l.peer_id, count(l.ipv4_dest_address) as num_prefixes_per_peer,
+       round(cast(sum(rtt_min) / count(rtt_min) as numeric), 2) as avg_rtt_min,
+       sum(pkt_loss) / count(pkt_loss) as avg_pkt_loss,
+       round(cast(sum(rtt_min) / count(rtt_min) as numeric), 2),
+       round(cast(sum(max_rtt) / count(max_rtt) as numeric), 2) as avg_max_rtt,
+       round(cast(sum(stddev_rtt) / count(stddev_rtt) as numeric), 2) as avg_stddev_rtt,
+       round(cast(sum(mediana_rtt) / count(mediana_rtt) as numeric), 2) as avg_mediana_rtt,
+       (sum(rtt_min + rtt_min * pkt_loss) / count(rtt_min)) as avg_cmpx_metric
+from pfr_log l
+join pfr_peers p on l.peer_id = p.id
+join node n on p.node_id = n.id 
+where probe_id in (select max(probe_id) from pfr_log l) group by n.hostname, l.peer_id;
+
+/*
+hostname | peer_id | num_prefixes_per_peer | avg_rtt_min | avg_pkt_loss | round | avg_max_rtt | avg_stddev_rtt | avg_mediana_rtt | avg_cmpx_metric  
+----------+---------+-----------------------+-------------+--------------+-------+-------------+----------------+-----------------+------------------
+ IPFIX1   |      10 |                     6 |       50.52 |            1 | 50.52 |       53.65 |           0.37 |           51.84 | 113.608498086532
+ IPFIX0   |       9 |                     2 |       77.32 |            1 | 77.32 |       79.78 |           0.24 |           79.12 | 154.641998291016
+ PE0      |       6 |                     3 |       74.98 |            2 | 74.98 |       76.07 |           0.16 |           76.07 | 224.944999694824
+ PE1      |       4 |                     6 |       65.03 |            2 | 65.03 |       67.48 |           0.35 |           66.41 | 170.369164943695
+ PE1      |       8 |                     4 |       57.87 |            1 | 57.87 |       59.20 |           0.13 |           58.81 | 116.617000699043
+ PE0      |       2 |                     4 |       75.96 |            1 | 75.96 |       80.38 |           0.47 |           78.88 | 209.330244064331
+ PE1      |       7 |                     2 |       40.38 |            2 | 40.38 |       40.84 |           0.09 |           40.57 | 158.062999486923
+(7 rows)
+*/
+
+-- 9. This query shows num prefixes per peer w/stats/metric  and round which pfrd send to gobgpd
+select n.hostname, l.peer_id, count(l.ipv4_dest_address) as num_prefixes_per_peer,
+       round(cast(sum(rtt_min) / count(rtt_min) as numeric), 2) as avg_rtt_min,
+       sum(pkt_loss) / count(pkt_loss) as avg_pkt_loss,
+       round(cast(sum(rtt_min) / count(rtt_min) as numeric), 2),
+       round(cast(sum(max_rtt) / count(max_rtt) as numeric), 2) as avg_max_rtt,
+       round(cast(sum(stddev_rtt) / count(stddev_rtt) as numeric), 2) as avg_stddev_rtt,
+       round(cast(sum(mediana_rtt) / count(mediana_rtt) as numeric), 2) as avg_mediana_rtt,
+       round(cast((sum(rtt_min + rtt_min * pkt_loss) / count(rtt_min)) as numeric), 2) as avg_cmpx_metric
+from pfr_log l
+join pfr_peers p on l.peer_id = p.id
+join node n on p.node_id = n.id
+where probe_id in (select max(probe_id) from pfr_log l) group by n.hostname, l.peer_id;
+
+/*
+ hostname | peer_id | num_prefixes_per_peer | avg_rtt_min | avg_pkt_loss | round | avg_max_rtt | avg_stddev_rtt | avg_mediana_rtt | avg_cmpx_metric 
+----------+---------+-----------------------+-------------+--------------+-------+-------------+----------------+-----------------+-----------------
+ IPFIX1   |      10 |                     6 |       76.10 |            1 | 76.10 |       78.70 |           0.37 |           77.85 |          151.02
+ IPFIX0   |       9 |                     4 |       75.69 |            1 | 75.69 |       78.65 |           0.32 |           77.66 |          172.24
+ PE0      |       6 |                     4 |       58.99 |            1 | 58.99 |       61.72 |           0.25 |           60.27 |          138.14
+ PE1      |       4 |                     3 |       77.10 |            1 | 77.10 |       79.40 |           0.30 |           78.88 |          157.53
+ PE0      |       2 |                     3 |       55.01 |            0 | 55.01 |       57.68 |           0.24 |           57.25 |           82.73
+ PE0      |       5 |                     2 |       75.20 |            1 | 75.20 |       79.40 |           0.38 |           78.17 |          190.72
+ PE1      |       7 |                     5 |       30.87 |            1 | 30.87 |       33.08 |           0.24 |           31.84 |           48.16
+(7 rows)
+*/
